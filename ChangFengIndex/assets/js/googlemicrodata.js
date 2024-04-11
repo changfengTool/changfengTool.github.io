f29fc4dd236ec5d1d35136922adf8eab1e63f2c4
fudanneko <nuttertools5790@gmail.com>
2024-04-11 11:10:15 +0800

    google結構化資料

---
+++ ChangFengIndex/assets/js/googlemicrodata.js
@@ -0,0 +1,40 @@
+<!--google結構化資料-->
+var structuredData = {
+    organization: {
+        "@type": "Organization",
+        "name": "昶鋒刀具有限公司",
+        "description": "昶鋒刀具有限公司，成立於104年，全體經營團隊及專業技師皆延續自前身昶奕企業社。在業界，昶奕企業社自93年起13年來專業製刀.破碎刀再生.焊補.破碎機器維修.經歷及實作經驗已將團隊實力提升到專業程度。",
+        "address": {
+            "@type": "PostalAddress",
+            "streetAddress": "台中市神岡區大圳路42-13號",
+            "addressLocality": "台中市",
+            "addressRegion": "神岡區",
+            "postalCode": "",
+            "addressCountry": "台灣"
+        },
+        "telephone": "04-25620972"
+    },
+    product: {
+        "@type": "Product",
+        "name": "破碎刀",
+        "description": "Your product description",
+        "offers": {
+            "@type": "Offer",
+            "price": "Your product price",
+            "priceCurrency": "Your currency"
+        }
+    },
+    service: {
+        "@type": "Service",
+        "name": "Your service name",
+        "description": "Your service description",
+        "offers": {
+            "@type": "Offer",
+            "price": "Your service price",
+            "priceCurrency": "Your currency"
+        }
+    }
+};
+
+
+<!--google結構化資料-->
